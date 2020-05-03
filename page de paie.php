817c71dd9c1936edbe8eebdbb5b0fb94b3b6e171
choudja07 <64559693+choudja07@users.noreply.github.com>
2020-05-03 14:21:37 +0100

    Add files via upload

---
+++ page de paie.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>paie</title>
+    <link rel="stylesheet" type="text/css" href="paiement.css">
+</head>
+
+<style type="text/css">
+ .se
+    {
+        float: right;
+        background-color: blue;
+        color: white;
+        width: 95px;
+        border-radius: 4px;
+        height: 22px;
+        border: blue;
+    }
+    .or
+    {
+        height: 24px;
+    }
+    p
+    {
+        text-align: center;
+    }
+    p a
+    {
+        color: lawngreen;
+    }
+    .in
+    {
+        background-color: black;
+        color: white;
+        border-top: black;
+        border-left: black;
+        border-right:black;
+        border-bottom: white;
+        width: 263px;
+
+    }
+
+    .su
+    {
+        background-color: red;
+        color: white;
+        width: 189px;
+        margin-left: 37px;
+        border-radius: 19px;
+        border: red;
+        height: 22px;
+
+    }
+    
+.se
+    {
+        float: right;
+        background-color: blue;
+        color: white;
+        width: 95px;
+        border-radius: 4px;
+        height: 22px;
+        border: blue;
+    }
+    .or
+    {
+        height: 24px;
+    }
+    p
+    {
+        text-align: center;
+    }
+    p a
+    {
+        color: lawngreen;
+    }
+    .diva
+    {
+        color: red;
+        background-color: white;
+    }
+</style>
+
+<body>
+
+        
+        
+    <br>
+ 
+
+
+</body>
+</html>
+
+
